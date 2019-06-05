bb718396ed45bb3ed75d842d368eab059155efb4
sasinskil <sasinskil@gmail.com>
2019-06-05 19:05:26 +0200

    prepare project, initial commit

---
+++ rss_atom_reader_client/src/components/News.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="news">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'news',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
